435595de1ee5686614c1cf57613eee643d7db1bd
Alan Le <alanle0824@gmail.com>
2018-02-08 18:33:43 -0800

    search bar and api request

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '8c025e5eb2879baf30935c37e3417bf2';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`; // use `` ES6 template strings
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
